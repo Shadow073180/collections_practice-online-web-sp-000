58452ecb92d668a9e7924809efad2c56447e04fc
David masterson <david73180@gmail.com>
2018-10-07 05:54:44 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,16 +1,5 @@
 def sort_array_asc(array)
-  new = []
-  array.each do |x|
-    x.to_i << new
-  end
-  new.sort do |a, b|
-    if a == b
-      0
-    elsif a < b
-      1
-    elsif a > b
-      -1
-    end
-    return new
+  array.each do |a, b|
+    a <=> b 
   end
 end
