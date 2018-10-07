fbc36b56f78559cc65f51d74fffc3df89346a3d8
David masterson <david73180@gmail.com>
2018-10-07 05:51:44 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
-
+def sort_array_asc(array)
+  new = []
+  array.each do |x|
+    x << new
+  end
+  new.sort do |a, b|
+    if a == b 
+      0
+    elsif a < b 
+      1
+    elsif a > b 
+      -1
+    end
+    return new
+  end
